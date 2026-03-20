9f64991fca6c88fb1da0700dfa179e292f5ddf74
Ponpreeth <ponpreeth@gmail.com>
2026-03-20 13:19:39 +0530

    Initial commit - Expense Tracker Project

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Expense = require("./models/Expense");
+
+const app = express();
+
+// MongoDB connection
+mongoose.connect("mongodb://127.0.0.1:27017/expenseDB")
+.then(() => console.log("MongoDB connected ✅"))
+.catch(err => console.log(err));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.redirect("/expenses");
+});
+
+app.get("/add-test", async (req, res) => {
+    const newExpense = new Expense({
+        title: "Test Expense",
+        amount: 100,
+        category: "Test"
+    });
+
+    await newExpense.save();
+
+    res.send("Test data saved ✅");
+});
+
+
+app.get("/expenses", async (req, res) => {
+    const expenses = await Expense.find();
+    res.render("expenses", { expenses });
+});
+
+app.get("/add", (req, res) => {
+    res.render("add");
+});
+
+app.get("/edit/:id", async (req, res) => {
+    const expense = await Expense.findById(req.params.id);
+    res.render("edit", { expense });
+});
+
+app.post("/add", async (req, res) => {
+    const { title, amount, category } = req.body;
+
+    const newExpense = new Expense({
+        title,
+        amount,
+        category
+    });
+
+    await newExpense.save();
+
+    res.redirect("/expenses");
+});
+
+app.post("/delete/:id", async (req, res) => {
+    const id = req.params.id;
+
+    await Expense.findByIdAndDelete(id);
+
+    res.redirect("/expenses");
+});
+
+app.post("/edit/:id", async (req, res) => {
+    const { title, amount, category } = req.body;
+
+    await Expense.findByIdAndUpdate(req.params.id, {
+        title,
+        amount,
+        category
+    });
+
+    res.redirect("/expenses");
+});
+
+app.set("view engine", "ejs");
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
+
+
+
